83bea9362d8921ddb667342b1638bd11d22c024e
sarracent <damiansarracent89@gmail.com>
2019-04-06 01:00:18 -0300

    first commit

--- src/main/java/com/sondeos/blog/config/CacheConfiguration.java
+++ src/main/java/com/sondeos/blog/config/CacheConfiguration.java
@@ -38,6 +38,11 @@ public class CacheConfiguration {
             cm.createCache(com.sondeos.blog.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.sondeos.blog.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.sondeos.blog.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.sondeos.blog.domain.Blog.class.getName(), jcacheConfiguration);
+            cm.createCache(com.sondeos.blog.domain.Entry.class.getName(), jcacheConfiguration);
+            cm.createCache(com.sondeos.blog.domain.Entry.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.sondeos.blog.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.sondeos.blog.domain.Tag.class.getName() + ".entries", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
